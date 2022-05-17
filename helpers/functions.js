ea6f1f2713c4f0ed95c6a01c1aba183dffb305b4
chicodevbr <chicodevbr@gmail.com>
2022-05-17 18:44:48 -0300

    feat: :sparkles: add resolvers and mutations

---
+++ helpers/functions.js
@@ -0,0 +1,9 @@
+function convertToSlug(Text) {
+  return Text.toLowerCase()
+    .replace(/[^\w ]+/g, '')
+    .replace(/ +/g, '-');
+}
+
+module.exports = {
+  convertToSlug,
+};
--- resolvers/PostResolvers.js
+++ resolvers/PostResolvers.js
@@ -1,9 +1,32 @@
 const { Post } = require('../models');
+const { convertToSlug } = require('../helpers/functions');
 
 const postResolver = {
   Query: {
     async posts() {
       return await Post.find({});
+    },
+    async post(_, { id }) {
+      return await Post.findById(id);
+    },
+  },
+  Mutation: {
+    createPost(_, { post }) {
+      const slug = convertToSlug(post.title);
+      const updatePost = { ...post, slug };
+
+      const newPost = new Post(updatePost);
+      return newPost.save();
+    },
+    async updatePost(_, { id, post }) {
+      return await Post.findByIdAndUpdate(id, post, {
+        new: true,
+      });
+    },
+    async deletePost(_, { id }) {
+      const isExist = await Post.findById(id);
+
+      return await Post.findByIdAndRemove(id);
     },
   },
 };
